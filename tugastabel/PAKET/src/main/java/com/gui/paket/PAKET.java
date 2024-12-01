d9e99cedb9e33561391b59cb1935147da22a5b40
MRYDXXZ <147012840+MRYDXXZ@users.noreply.github.com>
2024-12-01 20:55:03 +0700

    Add files via upload

---
+++ tugastabel/PAKET/src/main/java/com/gui/paket/PAKET.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gui.paket;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PAKET {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
